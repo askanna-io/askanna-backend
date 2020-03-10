1ecadb6264e95d5dc172dbb762bacb08624da44b
Anthony Leung <anthony@askanna.io>
2020-03-10 15:40:03 +0100

    [IMP] - #60 - Make packages and project endpoints in api

--- askanna_backend/project/admin.py
+++ askanna_backend/project/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from project.models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
