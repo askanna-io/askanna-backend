aaed8b64430f0abc03722544a5cf70ed1afcaba3
Anthony Leung <anthony@askanna.io>
2020-03-19 08:00:27 +0100

    [MRG] - #66 - Merge with master

--- askanna_backend/project/api/serializers.py
+++ askanna_backend/project/api/serializers.py
@@ -6,8 +6,4 @@ from project.models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-<<<<<<< HEAD
         fields = '__all__'
-=======
-        fields = '__all__'
->>>>>>> origin/master
