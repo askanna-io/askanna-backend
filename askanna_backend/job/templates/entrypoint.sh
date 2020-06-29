b6c968f28d8c85025929ee1611e7893e0b1de1ae
Anthony Leung <anthony@askanna.io>
2020-06-29 07:33:01 +0200

    [IMP] - #155 - first implementation for failing jobs

--- askanna_backend/job/templates/entrypoint.sh
+++ askanna_backend/job/templates/entrypoint.sh
@@ -12,10 +12,32 @@ cd /code
 
 echo 'AskAnna is running for project "{{pr.title}}" and running on "{{pr.short_uuid}}"'
 
+last_status=0
+
 # going into user land
 {% for command in commands %}
 echo '$ {{ command.print_command|safe }}'
 {{ command.command|safe }}
+
+last_status=$?
+
+if [ $last_status -neq 0 ]
+then
+  # AskAnna runner detected a non-zero exitcode and proceed with finishing uploading artificacts right now
+  # we don't store the result, as this job will not have any valid results because of the crash we just detected
+  echo "AskAnna exit_code=${last_status}"
+
+  # let's store the artifact for this run and exit
+  askanna artifact
+
+  # exit with stame status as the crashed job
+  exit $last_status
+
+else
+  # when everything was was fine, let's add an empty row in the result
+  echo ""
+fi
+
 {% endfor %}
 # end userland
 
