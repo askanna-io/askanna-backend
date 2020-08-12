f4fcaec7a149ce6c60c7d5e8920b888e02b47cc1
Anthony Leung <anthony@askanna.io>
2020-08-12 08:17:49 +0200

    [IMP] - #188 - Remove title field from project and created DescriptionModel

--- askanna_backend/core/models.py
+++ askanna_backend/core/models.py
@@ -3,20 +3,40 @@ import uuid
 from .utils import GoogleTokenGenerator, bx_decode
 
 from django.db import models
-from django_extensions.db.models import ActivatorModel, TimeStampedModel, TitleDescriptionModel
+from django.utils.translation import ugettext_lazy as _
+from django_extensions.db.models import (
+    ActivatorModel,
+    TimeStampedModel,
+    TitleDescriptionModel,
+)
 
 
 class DeletedModel(models.Model):
     """
     Defines an additional field to registere when the model is deleted
     """
-    deleted = models.DateTimeField(blank=True, auto_now_add=False, auto_now=False, null=True)
+
+    deleted = models.DateTimeField(
+        blank=True, auto_now_add=False, auto_now=False, null=True
+    )
+
     class Meta:
         abstract = True
 
+
+class DescriptionModel(models.Model):
+
+    description = models.TextField(_("description"), blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
 class SlimBaseModel(TimeStampedModel, DeletedModel, models.Model):
 
-    uuid = models.UUIDField(primary_key=True, db_index=True, editable=False, default=uuid.uuid4)
+    uuid = models.UUIDField(
+        primary_key=True, db_index=True, editable=False, default=uuid.uuid4
+    )
     short_uuid = models.CharField(max_length=32, blank=True)
 
     def save(self, *args, **kwargs):
@@ -26,22 +46,26 @@ class SlimBaseModel(TimeStampedModel, DeletedModel, models.Model):
         if not self.short_uuid and self.uuid:
             # FIXME: mode this part of code outside save to check for potential collision in existing set
             google_token = GoogleTokenGenerator()
-            self.short_uuid = google_token.create_token(key='', uuid=self.uuid)
+            self.short_uuid = google_token.create_token(key="", uuid=self.uuid)
         super().save(*args, **kwargs)
 
     class Meta:
         abstract = True
 
+
 class SlimBaseForAuthModel(SlimBaseModel):
 
     uuid = models.UUIDField(db_index=True, editable=False, default=uuid.uuid4)
+
     class Meta:
         abstract = True
+
 
 class BaseModel(TitleDescriptionModel, SlimBaseModel):
     class Meta:
         abstract = True
-        ordering = ['-modified']
+        ordering = ["-modified"]
+
 
 class ActivatedModel(ActivatorModel, BaseModel):
     class Meta:
