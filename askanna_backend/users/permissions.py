d1151a11dc950c439c108b0aea35b46307fcc037
caya <caya@askanna.io>
2020-08-26 10:07:09 +0200

    check user against role

--- askanna_backend/users/permissions.py
+++ askanna_backend/users/permissions.py
@@ -1,19 +1,20 @@
 from django.contrib.auth.models import Group
 from rest_framework import permissions
+from users.models import Membership
 
 
-def _is_in_group(user, group_name):
+def _is_in_group(user, role):
     """
-    Takes a user and a group name, and returns `True` if the user is in that group.
+    Takes a user and a role name, and returns `True` if the user is in that group.
     """
     try:
-        return Group.objects.get(name=group_name).user_set.filter(id=user.id).exists()
-    except Group.DoesNotExist:
+        return Membership.objects.get(role=role).user_set.filter(user=user.user).exists()
+    except Membership.DoesNotExist:
         return None
 
 
 def _has_group_permission(user, required_roles):
-    return any([_is_in_group(user, group_name) for group_name in required_roles])
+    return any([_is_in_group(user, role) for role in required_roles])
 
 
 class IsAdminUser(permissions.BasePermission):
--- askanna_backend/users/serializers.py
+++ askanna_backend/users/serializers.py
@@ -37,3 +37,4 @@ class MembershipSerializer(serializers.ModelSerializer):
             "last_active": "",
         }
 
+
