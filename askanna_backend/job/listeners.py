4136df727efd95e6bda8f6b5d9dfef858bd7c9e0
Anthony Leung <anthony@askanna.io>
2020-10-07 11:25:13 +0200

    [FIX] - #218 - Fix reference to the path where an artifact is stored, now we can extract the artifact.

--- askanna_backend/job/listeners.py
+++ askanna_backend/job/listeners.py
@@ -17,10 +17,15 @@ from job.signals import artifact_upload_finish
 
 @receiver(artifact_upload_finish)
 def handle_upload(sender, signal, postheaders, obj, **kwargs):
+    """
+    After saving the artifact, we extract the contents of the artifact to
+    BLOB_ROOT, which is served on the CDN to allow us to retrieve the individual
+    files one by one.
+    """
     source_location = settings.ARTIFACTS_ROOT
     target_location = settings.BLOB_ROOT
 
-    source_path = os.path.join(source_location, obj.storage_location)
+    source_path = os.path.join(source_location, obj.storage_location, obj.filename)
     target_path = os.path.join(target_location, str(obj.uuid))
 
     with ZipFile(source_path) as zippackage:
