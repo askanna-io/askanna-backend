382062435a3cea5107db804d1a2c7c33b8f5fadf
Anthony Leung <anthony@askanna.io>
2020-11-05 16:21:55 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- askanna_backend/users/tests/test_api_users.py
+++ askanna_backend/users/tests/test_api_users.py
@@ -60,7 +60,7 @@ class TestUserListAPI(BaseUsers, APITestCase):
         response = self.client.get(self.url, format="json",)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_list_users_a_anonymous(self):
+    def test_list_users_anonymous(self):
         """
         We cannot list users as anonymous user
         """
