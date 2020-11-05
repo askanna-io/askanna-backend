c0f9401588737ac2653699ac773490f17e2fe3aa
Anthony Leung <anthony@askanna.io>
2020-11-05 10:31:28 +0100

    Add test for access to user list and user creation

--- askanna_backend/users/tests/test_api_users.py
+++ askanna_backend/users/tests/test_api_users.py
@@ -1,8 +1,7 @@
-import pytest
 from django.urls import reverse
+import pytest
 from rest_framework import status
 from rest_framework.test import APITestCase
-
 
 from workspace.models import Workspace
 from workspace.views import PersonViewSet
@@ -11,7 +10,7 @@ from users.models import User
 pytestmark = pytest.mark.django_db
 
 
-class TestUserAPI(APITestCase):
+class BaseUsers:
     @classmethod
     def setup_class(cls):
         cls.users = {
@@ -24,36 +23,117 @@ class TestUserAPI(APITestCase):
             "userB": User.objects.create(username="userB", email="userB@askanna.dev"),
         }
 
+    @classmethod
+    def teardown_class(cls):
+        """
+        Remove all the user instances we had setup for the test
+        """
+        for _, user in cls.users.items():
+            user.delete()
+
+
+class TestUserListAPI(BaseUsers, APITestCase):
+    """
+    Testing the list function for the /v1/accounts/
+    """
+
+    def setUp(self):
+        self.url = reverse("user-list", kwargs={"version": "v1"},)
+
     def test_list_users_as_admin(self):
         """
         We can list users as an admin user
         """
-        url = reverse("user-list", kwargs={"version": "v1"},)
-
         token = self.users["admin"].auth_token
         self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
 
-        response = self.client.get(url, format="json",)
+        response = self.client.get(self.url, format="json",)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_list_users_as_normaluser(self):
         """
         We can list users as a normal user
         """
-        url = reverse("user-list", kwargs={"version": "v1"},)
-
         token = self.users["userB"].auth_token
         self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
 
-        response = self.client.get(url, format="json",)
+        response = self.client.get(self.url, format="json",)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_list_users_a_anonymous(self):
         """
         We can list users as a normal user
         """
-        url = reverse("user-list", kwargs={"version": "v1"},)
-
-        response = self.client.get(url, format="json",)
+        response = self.client.get(self.url, format="json",)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
+class TestUserCreateAPI(BaseUsers, APITestCase):
+    """
+    Testing the create function for the /v1/accounts/
+    """
+
+    def setUp(self):
+        self.url = reverse("user-list", kwargs={"version": "v1"},)
+
+    def test_create_user_as_admin(self):
+        """
+        We can create an user as admin/superuser
+        """
+
+        token = self.users["admin"].auth_token
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
+        response = self.client.post(
+            self.url,
+            {
+                "password": "1234567890abcdef",
+                "email": "admin-test@askanna.dev",
+                "username": "admin-test@askanna.dev",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # cleanup via internal functions
+        User.objects.get(short_uuid=response.data["short_uuid"]).delete()
+
+    def test_create_user_as_normaluser(self):
+        """
+        We can create an user as normal user
+        """
+        # FIXME: should check the business rule for this particular test
+        token = self.users["userB"].auth_token
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
+        response = self.client.post(
+            self.url,
+            {
+                "password": "1234567890abcdef",
+                "email": "user-test@askanna.dev",
+                "username": "user-test@askanna.dev",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # cleanup via internal functions
+        User.objects.get(short_uuid=response.data["short_uuid"]).delete()
+
+    def test_create_user_as_anonymous(self):
+        """
+        We can create an user as anonymous
+        """
+        response = self.client.post(
+            self.url,
+            {
+                "password": "1234567890abcdef",
+                "email": "anon-test@askanna.dev",
+                "username": "anon-test@askanna.dev",
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # cleanup via internal functions
+        User.objects.get(short_uuid=response.data["short_uuid"]).delete()
