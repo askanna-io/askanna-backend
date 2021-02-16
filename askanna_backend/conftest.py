fd66d940db09507838ff2e69f8b047ca0df58618
Anthony Leung <anthony@askanna.io>
2021-02-16 16:00:49 +0100

    Remove old seting

--- askanna_backend/conftest.py
+++ askanna_backend/conftest.py
@@ -8,7 +8,6 @@ from askanna_backend.users.tests.factories import UserFactory
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-    settings.IN_TEST_MODE = True
 
 
 @pytest.fixture
