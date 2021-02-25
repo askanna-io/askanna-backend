036bb5e28f2a0b770baedb741cba0f256aed200b
Anthony Leung <anthony@askanna.io>
2021-02-25 16:23:05 +0100

    Add patch to workspace for title and description

--- askanna_backend/workspace/serializers.py
+++ askanna_backend/workspace/serializers.py
@@ -4,6 +4,13 @@ from workspace.models import Workspace
 
 
 class WorkspaceSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.save()
+        instance.refresh_from_db()
+        return instance
+
     class Meta:
         model = Workspace
         fields = "__all__"
