2bdde8821e18905f9e2599603ec0673616555d8d
Robbert <robbert@askanna.io>
2023-10-26 09:09:06 +0200

    Handle missing files

--- apps/storage/tests/test_api_storage_file.py
+++ apps/storage/tests/test_api_storage_file.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 
 from core.utils.suuid import create_suuid
+from storage.models import File
 from tests import AskAnnaAPITestCASE
 
 
@@ -58,30 +59,50 @@ class TestStorageFileInfo(BaseStorageFileAPI):
         response = self.client.get(reverse("storage-file-info", kwargs={"version": "v1", "suuid": create_suuid()}))
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
+    def test_get_file_info_not_existing_file(self):
+        self.set_authorization(self.users["workspace_admin"])
+        file_object = self.avatar_files["workspace_admin"].first()
+
+        response = self.client.get(reverse("storage-file-info", kwargs={"version": "v1", "suuid": file_object.suuid}))
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["suuid"] == file_object.suuid  # type: ignore
+        assert response.data["download_info"] is not None  # type: ignore
+        assert response.data["download_info"]["type"] == "askanna"  # type: ignore
+
+        file_object.file.storage.delete(file_object.file.file.name)
+        assert File.objects.filter(suuid=file_object.suuid).exists() is True
+        assert file_object.file.storage.exists(file_object.file.name) is False
+
+        response = self.client.get(reverse("storage-file-info", kwargs={"version": "v1", "suuid": file_object.suuid}))
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
 
 class TestStorageFileDownload(BaseStorageFileAPI):
     def test_get_file_download(self):
         self.set_authorization(self.users["workspace_admin"])
-        file_suuid = self.avatar_files["workspace_admin"].first().suuid
-        response = self.client.get(reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_suuid}))
+        file_object = self.avatar_files["workspace_admin"].first()
+
+        response = self.client.get(
+            reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_object.suuid})
+        )
         assert response.status_code == status.HTTP_200_OK
         assert response.streaming_content is not None  # type: ignore
         assert response.get("Content-Disposition").startswith("attachment; filename=")  # type: ignore
-        assert response.get("Content-Type") == str(self.avatar_files["workspace_admin"].first().file.file.content_type)
-        assert response.get("Content-Length") == str(self.avatar_files["workspace_admin"].first().file.file.size)
+        assert response.get("Content-Type") == str(file_object.file.file.content_type)
+        assert response.get("Content-Length") == str(file_object.file.file.size)
 
     def test_get_file_download_set_content_disposition_type(self):
         self.set_authorization(self.users["workspace_admin"])
-        file_suuid = self.avatar_files["workspace_admin"].first().suuid
+        file_object = self.avatar_files["workspace_admin"].first()
         response = self.client.get(
-            reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_suuid}),
+            reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_object.suuid}),
             HTTP_RESPONSE_CONTENT_DISPOSITION="inline",
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.streaming_content is not None  # type: ignore
         assert response.get("Content-Disposition").startswith("inline; filename=")  # type: ignore
-        assert response.get("Content-Type") == str(self.avatar_files["workspace_admin"].first().file.file.content_type)
-        assert response.get("Content-Length") == str(self.avatar_files["workspace_admin"].first().file.file.size)
+        assert response.get("Content-Type") == str(file_object.file.file.content_type)
+        assert response.get("Content-Length") == str(file_object.file.file.size)
 
     def test_get_file_download_set_content_disposition_filename(self):
         self.set_authorization(self.users["workspace_admin"])
@@ -98,20 +119,17 @@ class TestStorageFileDownload(BaseStorageFileAPI):
 
     def test_get_file_download_set_range(self):
         self.set_authorization(self.users["workspace_admin"])
-        file_suuid = self.avatar_files["workspace_admin"].first().suuid
+        file_object = self.avatar_files["workspace_admin"].first()
         response = self.client.get(
-            reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_suuid}),
+            reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_object.suuid}),
             HTTP_RANGE="bytes=0-10",
         )
         assert response.status_code == status.HTTP_206_PARTIAL_CONTENT
         assert response.streaming_content is not None  # type: ignore
         assert response.get("Content-Disposition").startswith("inline; filename=")  # type: ignore
-        assert response.get("Content-Type") == str(self.avatar_files["workspace_admin"].first().file.file.content_type)
+        assert response.get("Content-Type") == str(file_object.file.file.content_type)
         assert response.get("Content-Length") == str(11)
-        assert (
-            response.get("Content-Range")
-            == f"bytes 0-10/{self.avatar_files['workspace_admin'].first().file.file.size}"
-        )
+        assert response.get("Content-Range") == f"bytes 0-10/{file_object.file.file.size}"
 
     def test_get_file_download_set_range_invalid_header(self):
         self.set_authorization(self.users["workspace_admin"])
@@ -142,4 +160,26 @@ class TestStorageFileDownload(BaseStorageFileAPI):
     def test_get_file_download_as_anonymous_not_existing_suuid(self):
         self.client.credentials()  # type: ignore
         response = self.client.get(reverse("storage-file-download", kwargs={"version": "v1", "suuid": create_suuid()}))
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_get_file_download_not_existing_file(self):
+        self.set_authorization(self.users["workspace_admin"])
+        file_object = self.avatar_files["workspace_admin"].first()
+
+        response = self.client.get(
+            reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_object.suuid})
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.streaming_content is not None  # type: ignore
+        assert response.get("Content-Disposition").startswith("attachment; filename=")  # type: ignore
+        assert response.get("Content-Type") == str(file_object.file.file.content_type)
+        assert response.get("Content-Length") == str(file_object.file.file.size)
+
+        file_object.file.storage.delete(file_object.file.file.name)
+        assert File.objects.filter(suuid=file_object.suuid).exists() is True
+        assert file_object.file.storage.exists(file_object.file.name) is False
+
+        response = self.client.get(
+            reverse("storage-file-download", kwargs={"version": "v1", "suuid": file_object.suuid})
+        )
         assert response.status_code == status.HTTP_404_NOT_FOUND
